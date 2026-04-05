4448aaff5cc05f994b96ef21fae290fea0320c48
Mario Schober <mario.schober@dynatrace.com>
2026-04-05 02:33:56 +0200

    clean up stuff

--- scripts/remove-pvs.sh
+++ scripts/remove-pvs.sh
@@ -6,11 +6,11 @@ KUBECONFIG="${KUBECONFIG:-tofu/kubernetes/output/kube-config.yaml}"
 echo "Removing finalizers from all PVs and deleting them..."
 
 for pv in $(kubectl --kubeconfig "$KUBECONFIG" get pv -o jsonpath='{.items[*].metadata.name}'); do
+  echo "Deleting PV: $pv"
+  kubectl --kubeconfig "$KUBECONFIG" delete pv "$pv" --wait=false
+
   echo "Patching finalizers on PV: $pv"
   kubectl --kubeconfig "$KUBECONFIG" patch pv "$pv" -p '{"metadata":{"finalizers":null}}' --type=merge
-
-  echo "Deleting PV: $pv"
-#  kubectl --kubeconfig "$KUBECONFIG" delete pv "$pv" --wait=false
 done
 
 echo "Done."
